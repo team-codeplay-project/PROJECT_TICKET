7b667aa42ce9bf70947487e85d1aaec6d6d34cb3
arypte <wjs505@gmail.com>
2023-06-11 17:32:10 +0900

    ver2.0
    nft.sol
    withdraw - 출금
    set_price - 티켓 가격 설정
    refund - 환불 ( burn 사용 )
    use - 티켓사용 ( bonues_token 1개 지급 )
    
    bonus_token.sol -- erc20
    t_mint( 사용 확인되면 토큰 1개 지급 )

--- solidity/contracts/bonus_token.sol
+++ solidity/contracts/bonus_token.sol
@@ -4,16 +4,20 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "./nft.sol" ;
 
 contract bonus_token is ERC20 , ERC20Burnable {
 
     address owner ;
-    
+    NFT_c nft_contract ;
+    address n_c_a ;
+     
     mapping( uint => uint ) R_BAL ; // 래플 밸런스
     mapping( uint => bool ) R_END ; // 래플 종료
     mapping( uint => address ) A_ADD ; // 옥션 최고 지값, 밸류
     mapping( uint => uint ) A_BAL ;
     mapping( uint => bool ) A_END ; // 옥션 종료
+    // 옥션 미구현
 
     constructor () ERC20("AToken", "AT") {
 
@@ -33,7 +37,15 @@ contract bonus_token is ERC20 , ERC20Burnable {
 
     }
 
-    function mint( address _to ) public chk_owner { // 사용 확인되면 토큰 1개 지급
+    function set_n_c( address add ) public {
+
+        n_c_a = add ;
+        nft_contract = NFT_c( n_c_a ) ;
+
+    }
+
+    function t_mint( address _to ) public { // 사용 확인되면 토큰 1개 지급
+        require( msg.sender == n_c_a ) ;
         _mint( _to , 1 ) ;
     }
 
@@ -52,23 +64,6 @@ contract bonus_token is ERC20 , ERC20Burnable {
         require( R_END[ _idx ] == false ) ;
         _transfer( address( this ) , _to , R_BAL[ _idx ] ) ;
         R_END[ _idx ] = true ;
-
-    }
-
-    function Auction( address _from , uint _idx ) public {
-
-        require( A_END[ _idx ] == false ) ;
-
-        _transfer( _from , address( this ) , 1 ) ; // 토큰 1개 수거
-        A_BAL[ _idx ] ++ ;
-
-    }
-
-    function Auction_END( uint _idx ) public chk_owner {
-
-        require( A_END[ _idx ] == false ) ;
-
-    
 
     }
 
