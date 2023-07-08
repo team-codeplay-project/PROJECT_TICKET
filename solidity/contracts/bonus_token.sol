502e59de82ac234d897e74b11c08e36a8a9f6d27
arypte <wjs505@gmail.com>
2023-07-08 21:31:07 +0900

    sol

--- solidity/contracts/bonus_token.sol
+++ solidity/contracts/bonus_token.sol
@@ -9,7 +9,6 @@ import "./nft.sol" ;
 contract bonus_token is ERC20("AToken", "AT") , ERC20Burnable , Ownable {
 
     NFT_c nft_contract ;
-    address n_c_a ;
 
     // OVERRIDE & REDEFINED FUNCTIONS
     function decimals() override public pure returns( uint8 ){
@@ -17,14 +16,11 @@ contract bonus_token is ERC20("AToken", "AT") , ERC20Burnable , Ownable {
     }
 
     function set_n_c( address add ) public onlyOwner(){
-
-        n_c_a = add ;
         nft_contract = NFT_c( n_c_a ) ;
-
     }
 
     function t_mint( address _to ) public { // 사용 확인되면 토큰 1개 지급
-        require( msg.sender == n_c_a ) ;
+        require( msg.sender == address( n_c_a ) ) ;
         _mint( _to , 1 ) ;
     }
 
@@ -34,8 +30,9 @@ contract bonus_token is ERC20("AToken", "AT") , ERC20Burnable , Ownable {
 
         // 참여조건 토큰 1개 보유
         // 토큰 1개 내야함
-  //      _burn( msg.sender , 1 ) ;
+        // burn( msg.sender , 1 ) ;
         emit Raffle( _n , msg.sender ) ;
+        
     }
 
     function Raffle_end( uint _n ) public  onlyOwner() returns( uint ) {
@@ -46,58 +43,4 @@ contract bonus_token is ERC20("AToken", "AT") , ERC20Burnable , Ownable {
 
     }
 
-/*
-    // 1안 구조체 써서 래플
-
-    struct Raffle {
-
-        mapping( uint => bool ) chk ; // 이번 래플에 참가 했는지 아닌지
-
-    }
-
-    Raffle[] public R_db ;
-    
-    //mapping( uint => mapping( uint => bool ) ) R_db ;
-    uint R_num ;
-
-    function R_insert() public { 
-        
-        for( uint i = 0 ; i < 1000 ; i ++ ) { 
-
-            //R_db[ l ].chk[ i ] = true ;
-            //R_db[ l ][ i ] = true ; // 661248 , 574998 , 553934 // 641583 , 557898 , 536834
-            R_db.push( ) ;
-         //   R_db[ i ].start_block = block.number ;
-
-        }
-
-    }
-
-    event R( uint a , uint b , uint c ) ;
-    function R_insert2() public { // 2705168 , 2352320 , 2331256 // 2705168 , 2352320 , 2331256
-
-        for( uint i = 0 ; i < 1000 ; i ++ ) {
-        emit R( i , 0 , 0 ) ;
-        }
-
-    }
-
-    function RAPPLE( address _from , uint _idx ) public {
-
-        //require( R_END[ _idx ] == false ) ;
-
-        _transfer( _from , address( this ) , 1 ) ; // 토큰 1개 수거
-        R_BAL[ _idx ] ++ ;
-
-    }
-
-    function RAPPLE_END( address _to , uint _idx ) public onlyOwner() {
-
-        // _to 는 js에서 랜덤 돌려서 당첨자 뽑기.
-        require( R_END[ _idx ] == false ) ;
-        _transfer( address( this ) , _to , R_BAL[ _idx ] ) ;
-        R_END[ _idx ] = true ;
-
-    }
-*/
 }
